478798dad7e62e0715af1cb7ec20c187514757ab
Chirag Parmar <chirag024parmar@gmail.com>
2022-07-16 21:02:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:Alan_music_app/pages/home_page.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alan Music app',
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
